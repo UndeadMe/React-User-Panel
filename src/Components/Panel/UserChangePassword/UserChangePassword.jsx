bf8f28ac2df9ea895a872e1ec53767c4d14fdeba
Banana021s <arash.k021@yahoo.com>
2022-05-28 23:14:31 +0430

    refactor the UserChangePassword comp to functional

--- src/Components/Panel/UserChangePassword/UserChangePassword.jsx
+++ src/Components/Panel/UserChangePassword/UserChangePassword.jsx
@@ -1,54 +1,51 @@
-import { Component } from 'react'
-
 // import other component
 import Titles from '../../Titles/Titles'
 import FormInput from '../../Forms/FormInput/FormInput'
+
 // import other pkg
 import { Form, Button, Col } from 'react-bootstrap'
 
-class UserChangePassword extends Component {
-    render() {
-        return (
-            <>
-                <Titles title="Welcome to the change password" text="change your password as you want" />
+const UserChangePassword = () => {
+    return (
+        <>
+            <Titles title="Welcome to the change password" text="change your password as you want" />
 
-                <Form className="mt-5">
-                    <FormInput 
-                        type="Password"
-                        className="p-0"
-                        inpClass='px-3 py-2'
-                        name="currentPassword"
-                        controlId="current-password-input"
-                        text="Current Password"
-                        placeholder="Enter your Current Password"
-                    />
-                    <FormInput 
-                        as={Col}
-                        type="password"
-                        inpClass='px-3 py-2'
-                        className="p-0 mt-3"
-                        name="newPassword"
-                        controlId="new-password-input"
-                        text="New Password"
-                        placeholder="Enter your New Password"
-                    />
-                    <FormInput 
-                        as={Col}
-                        type="password"
-                        inpClass='px-3 py-2'
-                        className="p-0 mt-3"
-                        name="ConfirmNewPassword"
-                        controlId="confirm-new-password-input"
-                        text="Confirm New Password"
-                        placeholder="Enter your Confirm New Password"
-                    />
-                    <Button variant="primary" className='mt-5 py-2 px-4' type="submit">
-                        Update
-                    </Button>
-                </Form>
-            </>
-        )
-    }
+            <Form className="mt-5">
+                <FormInput 
+                    type="Password"
+                    className="p-0"
+                    inpClass='px-3 py-2'
+                    name="currentPassword"
+                    controlId="current-password-input"
+                    text="Current Password"
+                    placeholder="Enter your Current Password"
+                />
+                <FormInput 
+                    as={Col}
+                    type="password"
+                    inpClass='px-3 py-2'
+                    className="p-0 mt-3"
+                    name="newPassword"
+                    controlId="new-password-input"
+                    text="New Password"
+                    placeholder="Enter your New Password"
+                />
+                <FormInput 
+                    as={Col}
+                    type="password"
+                    inpClass='px-3 py-2'
+                    className="p-0 mt-3"
+                    name="ConfirmNewPassword"
+                    controlId="confirm-new-password-input"
+                    text="Confirm New Password"
+                    placeholder="Enter your Confirm New Password"
+                />
+                <Button variant="primary" className='mt-5 py-2 px-4' type="submit">
+                    Update
+                </Button>
+            </Form>
+        </>
+    )
 }
 
 export default UserChangePassword
