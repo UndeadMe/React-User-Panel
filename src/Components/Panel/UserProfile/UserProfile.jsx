e277b67b77790884592e66caa01b46da3e3cda0e
Banana021s <arash.k021@yahoo.com>
2022-05-31 14:43:53 +0430

    refactor the UserProfile comp to functional comp

--- src/Components/Panel/UserProfile/UserProfile.jsx
+++ src/Components/Panel/UserProfile/UserProfile.jsx
@@ -6,30 +6,27 @@ import styles from './UserProfile.module.css'
 import { Import } from 'iconsax-react'
 import PropTypes from 'prop-types';
 
-class UserProfile extends PureComponent {
-    capitalizeText(text) {
+const UserProfile = ({ userProfile='img/Arash.jpg', userBirthday, username, userEmail }) => {
+    const capitalizeText = (text) => {
         const firstLetter = text.charAt(0).toUpperCase()
         const otherLetters = text.slice(1)
         return `${firstLetter}${otherLetters}`
     }
 
-    render() {
-        const { userProfile='img/Arash.jpg', userBirthday, username, userEmail } = this.props
-        return (
-            <div className={`${styles['user-profile']} d-flex flex-column align-items-center border bg-white`}>
-                <label htmlFor="user-profile" className={styles['user-profile-label']}>
-                    <img src={userProfile} alt="" />
-                    <div className={`${styles['profile-icon-box']} bg-primary`}>
-                        <Import size='20' color="white" />
-                    </div>
-                    <input type="file" className="d-none" id='user-profile' />
-                </label>
-                <h1 className={`${styles.username} mt-3`}> {this.capitalizeText(username)} </h1>
-                <h4 className={`${styles['user-birthday']} mt-1`}>🥳 {userBirthday} 🥳</h4>
-                <h4 className={`${styles['user-email']} mt-1`}>{this.capitalizeText(userEmail)}</h4>
-            </div>
-        )
-    }
+    return (
+        <div className={`${styles['user-profile']} d-flex flex-column align-items-center border bg-white`}>
+            <label htmlFor="user-profile" className={styles['user-profile-label']}>
+                <img src={userProfile} alt="" />
+                <div className={`${styles['profile-icon-box']} bg-primary`}>
+                    <Import size='20' color="white" />
+                </div>
+                <input type="file" className="d-none" id='user-profile' />
+            </label>
+            <h1 className={`${styles.username} mt-3`}> {capitalizeText(username)} </h1>
+            <h4 className={`${styles['user-birthday']} mt-1`}>🥳 {userBirthday} 🥳</h4>
+            <h4 className={`${styles['user-email']} mt-1`}>{capitalizeText(userEmail)}</h4>
+        </div>
+    )
 }
 
 // validate the component
