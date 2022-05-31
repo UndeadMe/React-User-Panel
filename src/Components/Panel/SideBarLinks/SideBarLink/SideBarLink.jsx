bfd264d53d76373dff05edf0d07d39fc912b183d
Banana021s <arash.k021@yahoo.com>
2022-05-31 14:53:42 +0430

    refactor SideBarLink comp to functional comp

--- src/Components/Panel/SideBarLinks/SideBarLink/SideBarLink.jsx
+++ src/Components/Panel/SideBarLinks/SideBarLink/SideBarLink.jsx
@@ -1,35 +1,31 @@
-import { Component } from 'react'
-
 // import styles of this component
 import styles from './SideBarLink.module.css'
+
 // import other pkg to use
 import { ArrowRight2 } from 'iconsax-react'
 import PropTypes from 'prop-types'
 
-class SideBarLink extends Component {
-    render() {
-        const { id, border, text, icon, href, active, onActive } = this.props 
-        return (
-            <>
-                <li className={`${styles['sidebar-link']} d-flex justify-content-between align-items-center px-4`} onClick={() => onActive(id)}>
-                    {href ? (
-                        <a href={href} className='d-flex justify-content-between w-100 align-items-center text-decoration-none text-black'>
-                            {icon}
-                            <span>{text}</span>
-                            <ArrowRight2 className={`${styles['right-arrow-icon']}`} size='20' color="black" />
-                        </a>
-                    ) : (
-                        <>
-                            {icon}
-                            <span className={active ? 'text-primary' : 'text-black'}>{text}</span>
-                            <ArrowRight2 className={`${styles['right-arrow-icon']} ${active && styles.active}`} size='20' color="black" />
-                        </>
-                    )}
-                </li>
-                {border && <div className={styles['sidebar-link-border']}><div></div></div>}
-            </>
-        )
-    }
+const SideBarLink = ({ id, border, text, icon, href, active, onActive }) => {
+    return (
+        <>
+            <li className={`${styles['sidebar-link']} d-flex justify-content-between align-items-center px-4`} onClick={() => onActive(id)}>
+                {href ? (
+                    <a href={href} className='d-flex justify-content-between w-100 align-items-center text-decoration-none text-black'>
+                        {icon}
+                        <span>{text}</span>
+                        <ArrowRight2 className={`${styles['right-arrow-icon']}`} size='20' color="black" />
+                    </a>
+                ) : (
+                    <>
+                        {icon}
+                        <span className={active ? 'text-primary' : 'text-black'}>{text}</span>
+                        <ArrowRight2 className={`${styles['right-arrow-icon']} ${active && styles.active}`} size='20' color="black" />
+                    </>
+                )}
+            </li>
+            {border && <div className={styles['sidebar-link-border']}><div></div></div>}
+        </>
+    )
 }
 
 // validate component
