dd063ce967b9af07b1e1c2b3ae8edd4a267386ad
Banana021s <arash.k021@yahoo.com>
2022-05-31 14:52:42 +0430

    refactor SideBarLinks comp to functional comp

--- src/Components/Panel/SideBarLinks/SideBarLinks.jsx
+++ src/Components/Panel/SideBarLinks/SideBarLinks.jsx
@@ -1,31 +1,27 @@
-import { Component } from "react";
+import { useState } from 'react'
 
 // import styles of this component
 import styles from './SideBarLinks.module.css'
+
 // import other component to use
 import SideBarLink from "./SideBarLink/SideBarLink";
+
 // import other pkg 
 import PropTypes from 'prop-types'
 
-class SideBarLinks extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            links: [
-                ...this.props.sidebarLinks 
-            ]
-        }
-        this.activeLink = this.activeLink.bind(this)
-    }
+const SideBarLinks = ({ sidebarLinks, onChangeToggle }) => {
+    const [linksState, setLinks] = useState({
+        links: [ ...sidebarLinks  ]
+    })
 
-    activeLink(linkId) {
-        this.state.links.forEach(link => link.active = false)
+    const activeLink = (linkId) => {
+        linksState.links.forEach(link => link.active = false)
         
-        const link = this.state.links.find(link => link.id === linkId)
+        const link = linksState.links.find(link => link.id === linkId)
         if (!link.href) {
             link.active = true
             
-            this.setState(prev => {
+            setLinks(prev => {
                 return {
                     links: [
                         ...prev.links
@@ -33,30 +29,28 @@ class SideBarLinks extends Component {
                 }
             })
 
-            this.props.onChangeToggle(link.text.toLowerCase())
+            onChangeToggle(link.text.toLowerCase())
         }
     }
 
-    render() {
-        return (
-            <div className={`${styles['sidebar-links']} mt-4 bg-white border px-2 py-4`}>
-                <ul className="m-0 p-0">
-                    {this.state.links.map(link => (
-                        <SideBarLink 
-                            key={link.id}
-                            id={link.id}
-                            href={link.href} 
-                            border={link.border}
-                            text={link.text} 
-                            icon={link.icon} 
-                            active={link.active} 
-                            onActive={this.activeLink}
-                        />
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div className={`${styles['sidebar-links']} mt-4 bg-white border px-2 py-4`}>
+            <ul className="m-0 p-0">
+                {linksState.links.map(link => (
+                    <SideBarLink 
+                        key={link.id}
+                        id={link.id}
+                        href={link.href} 
+                        border={link.border}
+                        text={link.text} 
+                        icon={link.icon} 
+                        active={link.active} 
+                        onActive={activeLink}
+                    />
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 // validate the component
