db9f9d80bd7e3e5d2de5bd66d4b70a253e81bdd8
Banana021s <arash.k021@yahoo.com>
2022-05-31 14:41:15 +0430

    refactor Title comp to functional comp

--- src/Components/Titles/Titles.jsx
+++ src/Components/Titles/Titles.jsx
@@ -1,20 +1,15 @@
-import React, { Component } from 'react'
-
 // import styles of this component
 import styles from './Titles.module.css'
 // import other pkg
 import PropTypes from 'prop-types'
 
-class Titles extends Component {
-    render() {
-        const { title, text } = this.props 
-        return (
-            <>
-                <h1 className={`${styles['information-heading']} mt-1`}>{title}</h1>
-                <h3 className={`${styles['information-heading-text']} text-muted p-0`}>{text}</h3>
-            </>
-        )
-    }
+const Titles = ({ title, text }) => {
+    return (
+        <>
+            <h1 className={`${styles['information-heading']} mt-1`}>{title}</h1>
+            <h3 className={`${styles['information-heading-text']} text-muted p-0`}>{text}</h3>
+        </>
+    )
 }
 
 // validate component
