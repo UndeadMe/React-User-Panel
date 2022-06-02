3dc16c01cdb2dae1c57491efc257e477a9422457
Banana021s <arash.k021@yahoo.com>
2022-06-03 01:50:20 +0430

    refactor the App.js to the functional comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Component } from 'react'
+import { useEffect, useState } from 'react';
 
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 // import my other component 
 import RegisterForm from './Components/Forms/RegisterForm/RegisterForm'
 import Panel from './Components/Panel/Panel';
@@ -10,52 +11,37 @@ import LoginForm from './Components/Forms/LoginForm/LoginForm';
 // import utils
 import { getStorage } from './utils/storage';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const App = () => {
+  const [toggle, setToggle] = useState('');
     
-    this.state = {
-      toggle: null
-    }
-
-    this.changeToggle = this.changeToggle.bind(this)
-    this.checkIsInitStorage = this.checkIsInitStorage.bind(this)
-    this.checkUserIsRegister = this.checkUserIsRegister.bind(this)
-  }
-
-  changeToggle(toggle) {
-    this.setState({ toggle })
-  }
-
-  checkUserIsRegister() {
-    if (this.checkIsInitStorage()) {
+  useEffect(() => {
+    checkUserIsRegister()
+    // eslint-disable-next-line
+  }, [])
+  
+  const changeToggle = (toggle) => setToggle(toggle)
+  
+  const checkIsInitStorage = () => getStorage('users') && getStorage('users').length !== 0
+  
+  const checkUserIsRegister = () => {
+    if (checkIsInitStorage()) {
       const userId = getStorage('id')
       const users = getStorage('users')
       
       const [ userRegistered ] = users.filter(user => user.id === userId)
-
-      userRegistered.isLogin && this.changeToggle('panel')
-      !userRegistered.isLogin && this.changeToggle('login')
-    } else this.changeToggle('register')
+      
+      userRegistered.isLogin && changeToggle('panel')
+      !userRegistered.isLogin && changeToggle('login')
+    } else changeToggle('register')
   }
-
-  checkIsInitStorage() {
-    return getStorage('users') && getStorage('users').length !== 0
-  }
-
-  componentDidMount() {
-    this.checkUserIsRegister()
-  }
-
-  render() {
-    return (
-      <>
-        { this.state.toggle === "register" && <RegisterForm onRegister={this.checkUserIsRegister}  onLogin={this.changeToggle} /> }
-        { this.state.toggle === 'login' && <LoginForm onRegister={this.changeToggle} onLogin={this.checkUserIsRegister} /> }
-        { this.state.toggle === 'panel' && <Panel onLogOut={this.checkUserIsRegister} /> }
-      </>
-    )
-  }
+  
+  return (
+    <>
+      { toggle === 'register' && <RegisterForm onRegister={checkUserIsRegister}  onLogin={changeToggle} /> }
+      { toggle === 'login' && <LoginForm onRegister={changeToggle} onLogin={checkUserIsRegister} /> }
+      { toggle === 'panel' && <Panel onLogOut={checkUserIsRegister} /> }
+    </>
+  )
 }
 
 export  default App
